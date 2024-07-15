d9f2d4786fec8d73e444172f99771728fe8c2981
JWest2323 <thejamalwest@gmail.com>
2024-07-14 19:53:29 -0500

    app component prefix change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BooksComponent } from './books/books.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'library-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BooksComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'library-app';
