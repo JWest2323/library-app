e208f34a5e7e53860c13fb5b1c242fdc53f1882e
JWest2323 <thejamalwest@gmail.com>
2024-07-14 22:22:58 -0500

    book component bookid update

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -12,24 +12,28 @@ import { BookDetails } from './books';
 export class BooksComponent {
   books: BookDetails[] = [
     {
+      bookid: "1",
       title: 'The Alchemist ',
       author: 'Paulo Coelho',
       yearPublished: new Date('04-25-1993').getFullYear(),
       genre: 'Fantasy Novel',
     },
     {
+      bookid: "2",
       title: 'The Richest Man in Babylon',
       author: 'George Samuel Clason',
       yearPublished: new Date('04-04-1926').getFullYear(),
       genre: 'Non-fiction/Personal Finance',
     },
     {
+      bookid: "3",
       title: 'The Magic of Thinking Big',
       author: 'David J. Schwartz',
       yearPublished: new Date('04-02-1987').getFullYear(),
       genre: 'Self-help',
     },
     {
+      bookid: "4",
       title: 'The Four Agreements: A Practical Guide to Personal Freedom',
       author: 'Don Miguel Ruiz',
       yearPublished: new Date('11-07-1997').getFullYear(),
