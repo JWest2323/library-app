72b08d44d3a2f573744e82fb85761b7d6f9124d1
JWest2323 <thejamalwest@gmail.com>
2024-07-18 15:08:20 -0400

    uuid for all bookid's and corrected removeBook logic

--- src/app/book/bookslist/bookslist.component.ts
+++ src/app/book/bookslist/bookslist.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { BookDetails } from '../book';
 import { CommonModule } from '@angular/common';
 import { BookFormComponent } from '../../book-form/book-form.component';
@@ -15,34 +15,36 @@ import { v4 as uuidv4 } from 'uuid';
 export class BookslistComponent {
   books: BookDetails[] = [
     {
-      bookid: '1',
+      bookid: uuidv4(),
       title: 'The Alchemist ',
       author: 'Paulo Coelho',
       yearPublished: new Date('04-25-1993').getFullYear(),
       genre: 'Fantasy Novel',
     },
     {
-      bookid: '2',
+      bookid: uuidv4(),
       title: 'The Richest Man in Babylon',
       author: 'George Samuel Clason',
       yearPublished: new Date('04-04-1926').getFullYear(),
       genre: 'Non-fiction/Personal Finance',
     },
     {
-      bookid: '3',
+      bookid: uuidv4(),
       title: 'The Magic of Thinking Big',
       author: 'David J. Schwartz',
       yearPublished: new Date('04-02-1987').getFullYear(),
       genre: 'Self-help',
     },
     {
-      bookid: '4',
+      bookid: uuidv4(),
       title: 'The Four Agreements: A Practical Guide to Personal Freedom',
       author: 'Don Miguel Ruiz',
       yearPublished: new Date('11-07-1997').getFullYear(),
       genre: 'Personal development, Spirituality, Self-help',
     },
   ];
+
+  selectedBookValue!: BookDetails;
 
   addBook(book: BookComponent) {
     // console.log(book);
@@ -51,13 +53,19 @@ export class BookslistComponent {
       title: book.title,
       author: book.author,
       yearPublished: book.yearPublished,
-      genre: book.genre
-    }
+      genre: book.genre,
+    };
 
     this.books = [...this.books, newBook];
   }
 
-  removeBook(currentBook: BookDetails) {
-    this.books = this.books.filter((book) => book.bookid != currentBook.bookid);
+  onDelete(currentBook: BookDetails) {
+    this.selectedBookValue = currentBook;
+  }
+
+  removeBook() {
+    this.books = this.books.filter(
+      (book) => book.bookid !== this.selectedBookValue.bookid
+    );
   }
 }
