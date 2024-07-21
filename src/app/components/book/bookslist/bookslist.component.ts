b60a3d2c77492463bdba8373d0af75b434151374
JWest2323 <thejamalwest@gmail.com>
2024-07-20 22:10:17 -0500

    update booklist component addBook function to use bookService GET call

--- src/app/components/book/bookslist/bookslist.component.ts
+++ src/app/components/book/bookslist/bookslist.component.ts
@@ -2,8 +2,6 @@ import { AfterContentInit, Component, OnInit } from '@angular/core';
 import { BookDetails } from '../book';
 import { CommonModule } from '@angular/common';
 import { AddBookFormComponent } from '../../book-forms/add-book-form/add-book-form.component';
-import { BookComponent } from '../book.component';
-import { v4 as uuidv4 } from 'uuid';
 import { EditBookFormComponent } from '../../book-forms/edit-book-form/edit-book-form.component';
 import { BookService } from '../../../services/book.service';
 
@@ -15,11 +13,10 @@ import { BookService } from '../../../services/book.service';
   styleUrl: './bookslist.component.scss',
 })
 export class BookslistComponent implements OnInit, AfterContentInit {
-  
   ngOnInit(): void {
     this.bookService.getBooks().subscribe((data) => {
       this.books = data;
-    })
+    });
   }
 
   ngAfterContentInit(): void {
@@ -33,22 +30,17 @@ export class BookslistComponent implements OnInit, AfterContentInit {
   }
 
   constructor(private bookService: BookService) {}
- 
+
   books: BookDetails[] = [];
 
   selectedBookValue!: BookDetails;
 
-  addBook(book: BookComponent) {
-    // console.log(book);
-    let newBook: BookDetails = {
-      bookid: uuidv4(),
-      title: book.title,
-      author: book.author,
-      yearPublished: book.yearPublished,
-      genre: book.genre,
-    };
-
-    this.books = [...this.books, newBook];
+  addBook(book: BookDetails) {
+    this.bookService.addBook(book).subscribe((data) => {
+      console.log(data);
+      
+      this.books = data;
+    });
   }
 
   onDelete(currentBook: BookDetails) {
