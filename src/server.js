1deb1241b23c175e9725e3f7a147816ff89941a7
JWest2323 <thejamalwest@gmail.com>
2024-07-20 19:19:23 -0500

    server code for CRUD operations

---
+++ src/server.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
+
+// init express app
+
+const app = express();
+const port = process.env.port || 3000;
+
+// bodyParser middleware
+app.use(bodyParser.json());
+
+// in-memory data storage
+let books = [
+  {
+    bookid: uuidv4(),
+    title: "The Alchemist ",
+    author: "Paulo Coelho",
+    yearPublished: new Date("04-25-1993").getFullYear(),
+    genre: "Fantasy Novel",
+  },
+  {
+    bookid: uuidv4(),
+    title: "The Richest Man in Babylon",
+    author: "George Samuel Clason",
+    yearPublished: new Date("04-04-1926").getFullYear(),
+    genre: "Non-fiction/Personal Finance",
+  },
+  {
+    bookid: uuidv4(),
+    title: "The Magic of Thinking Big",
+    author: "David J. Schwartz",
+    yearPublished: new Date("04-02-1987").getFullYear(),
+    genre: "Self-help",
+  },
+  {
+    bookid: uuidv4(),
+    title: "The Four Agreements: A Practical Guide to Personal Freedom",
+    author: "Don Miguel Ruiz",
+    yearPublished: new Date("11-07-1997").getFullYear(),
+    genre: "Personal development, Spirituality, Self-help",
+  },
+];
+
+// GET /books - Returns all books
+app.get("/books", (req, res) => {
+  res.json(books);
+});
+
+// POST /books - Adds a new book
+app.post("/books", (req, res) => {
+  const book = { id: uuidv4(), ...req.body };
+  books.push(book);
+  res.status(201).send(books);
+});
+
+// PUT /books/:id - Updates an existing book
+app.put("/books/:id", (req, res) => {
+  const id = req.params.id;
+  const bookIndex = books.findIndex((book) => book.id === id);
+
+  if (bookIndex === -1) {
+    return res.status(404).send({ error: "Book was not found" });
+  }
+
+  const updatedBook = { id, ...req.body };
+
+  books[bookIndex] = updatedBook;
+
+  res.send(updatedBook);
+});
+
+// DELETE /books/:id - Deletes an existing book
+app.delete("/books/:id", (req, res) => {
+  const id = req.params.id;
+  const bookIndex = books.findIndex((book) => book.id === id);
+
+  if (bookIndex === -1) {
+    res.status(404).send({ error: "Book was not found" });
+  }
+
+  const deletedBook = books.splice(bookIndex, 1);
+
+  res.send(deletedBook);
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+});
